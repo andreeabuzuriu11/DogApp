966ed6d89c4d9e9895367b921304ba77c429b779
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2022-01-08 18:15:12 +0200

    Show info into DogDetailActivity

--- app/src/main/java/com/buzuriu/dogapp/viewModels/DogDetailViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/viewModels/DogDetailViewModel.kt
@@ -1,20 +1,31 @@
 package com.buzuriu.dogapp.viewModels
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.buzuriu.dogapp.models.DogObj
+import com.buzuriu.dogapp.utils.ImageUtils
 
 class DogDetailViewModel : BaseViewModel() {
 
-    var dog = DogObj()
+    var dog = MutableLiveData<DogObj>()
+    var dogPlaceHolder : MutableLiveData<Drawable>
+   // var dogBitmap: MutableLiveData<Bitmap>
+
 
     init {
-        dog = dataExchangeService.get<DogObj>(this::class.java.name)!!
-        Log.d("andreea ageString= ", dog.toString())
+        dogPlaceHolder = MutableLiveData<Drawable>(getDogPlaceHolder())
+     //   dogBitmap =
 
+        dog.value = dataExchangeService.get<DogObj>(this::class.java.name)!!
 
+    }
 
-
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun getDogPlaceHolder(): Drawable? {
+        return activityService.activity!!.getDrawable(ImageUtils.getDogPlaceholder())
     }
 
 }
