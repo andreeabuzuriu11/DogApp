07f83e2fd67a7de9602323667c6867a91351b496
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2022-01-08 20:57:40 +0200

    Changed dog details xml (margin, padding and alignment)

--- app/src/main/java/com/buzuriu/dogapp/viewModels/DogDetailViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/viewModels/DogDetailViewModel.kt
@@ -1,9 +1,7 @@
 package com.buzuriu.dogapp.viewModels
 
 import android.annotation.SuppressLint
-import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.buzuriu.dogapp.models.DogObj
 import com.buzuriu.dogapp.utils.ImageUtils
@@ -12,13 +10,9 @@ class DogDetailViewModel : BaseViewModel() {
 
     var dog = MutableLiveData<DogObj>()
     var dogPlaceHolder : MutableLiveData<Drawable>
-   // var dogBitmap: MutableLiveData<Bitmap>
-
 
     init {
         dogPlaceHolder = MutableLiveData<Drawable>(getDogPlaceHolder())
-     //   dogBitmap =
-
         dog.value = dataExchangeService.get<DogObj>(this::class.java.name)!!
 
     }
