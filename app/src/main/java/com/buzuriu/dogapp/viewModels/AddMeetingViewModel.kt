f8e783dac0291f55ebcf8b275bcc49bf46790212
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2022-01-25 20:12:01 +0200

    Revert "Revert "Added xml for add new meeting and meeting cell""
    This reverts commit e4c3d5243801f1c29d9edb21fa7ba7d0ccb90f4c.

--- app/src/main/java/com/buzuriu/dogapp/viewModels/AddMeetingViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/viewModels/AddMeetingViewModel.kt
@@ -1,6 +1,26 @@
 package com.buzuriu.dogapp.viewModels
 
+import android.widget.TimePicker
+import androidx.lifecycle.MutableLiveData
+import com.buzuriu.dogapp.views.SelectBreedFragment
 import com.buzuriu.dogapp.views.main.ui.OverlayActivity
 
 class AddMeetingViewModel : BaseViewModel() {
+
+    val timePicker : MutableLiveData<TimePicker>? = null
+    var dog = MutableLiveData("")
+
+    fun selectDog() {
+       /* navigationService.showOverlay(
+            OverlayActivity::class.java,
+            false,
+            OverlayActivity.fragmentClassNameParam,
+            SelectDogFragment::class.qualifiedName
+        )
+        if (!dog.value.isNullOrEmpty())
+            dataExchangeService.put(
+                SelectBreedViewModel::class.qualifiedName!!,
+                dog.value.toString()
+            )*/
+    }
 }
