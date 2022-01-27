1ad4378630a50922f69ec04958a2411840a1d975
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2022-01-27 19:19:14 +0200

    Also added image next to dog name when selecting it

--- app/src/main/java/com/buzuriu/dogapp/viewModels/AddMeetingViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/viewModels/AddMeetingViewModel.kt
@@ -1,16 +1,26 @@
 package com.buzuriu.dogapp.viewModels
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.widget.TimePicker
 import androidx.lifecycle.MutableLiveData
+import com.buzuriu.dogapp.R
 import com.buzuriu.dogapp.models.BreedObj
 import com.buzuriu.dogapp.models.DogObj
+import com.buzuriu.dogapp.utils.ImageUtils
 import com.buzuriu.dogapp.views.SelectDogFragment
 import com.buzuriu.dogapp.views.main.ui.OverlayActivity
 
 class AddMeetingViewModel : BaseViewModel() {
 
     val timePicker : MutableLiveData<TimePicker>? = null
-    var dog = MutableLiveData("")
+    var dogName = MutableLiveData("")
+    var dog = MutableLiveData<DogObj>()
+    var dogPlaceHolder: MutableLiveData<Drawable>
+
+    init {
+        dogPlaceHolder = MutableLiveData<Drawable>(getDogPlaceHolder())
+    }
 
     fun selectDog() {
         navigationService.showOverlay(
@@ -19,17 +29,23 @@ class AddMeetingViewModel : BaseViewModel() {
             OverlayActivity.fragmentClassNameParam,
             SelectDogFragment::class.qualifiedName
         )
-        if (!dog.value.isNullOrEmpty())
+        if (!dogName.value.isNullOrEmpty())
             dataExchangeService.put(
                 SelectDogViewModel::class.qualifiedName!!,
-                dog.value.toString()
+                dogName.value.toString()
             )
     }
 
     override fun onResume() {
         val selectedDog = dataExchangeService.get<DogObj>(this::class.qualifiedName!!)
         if (selectedDog != null) {
-            dog.value = selectedDog.name
+            dogName.value = selectedDog.name
+            dog.value = selectedDog!!
         }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun getDogPlaceHolder(): Drawable? {
+        return activityService.activity!!.getDrawable(R.drawable.ic_dog_svgrepo_com)
     }
 }
