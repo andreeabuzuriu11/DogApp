8acf619547c7a7463834caa68f18101051ecec1d
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2022-04-28 11:42:04 +0300

    When user wants to add a new meeting and only has 1 dog, that dog will automatically be selected

--- app/src/main/java/com/buzuriu/dogapp/viewModels/AddMeetingViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/viewModels/AddMeetingViewModel.kt
@@ -36,6 +36,16 @@ class AddMeetingViewModel : BaseViewModel() {
         dogPlaceHolder = MutableLiveData<Drawable>(getDogPlaceHolder())
         datePickerCalendar.value = Calendar.getInstance()
         timePickerCalendar.value = Calendar.getInstance()
+
+        if (doesUserHaveOnlyOneDog())
+            dog.value = localDatabaseService.get<ArrayList<DogObj>>("localDogsList")?.get(0)
+    }
+
+    override fun onResume() {
+        val selectedDog = dataExchangeService.get<DogObj>(this::class.qualifiedName!!)
+        if (selectedDog != null) {
+            dog.value = selectedDog!!
+        }
     }
 
     fun selectDog() {
@@ -50,23 +60,6 @@ class AddMeetingViewModel : BaseViewModel() {
                 SelectDogViewModel::class.qualifiedName!!,
                 dog.value?.name.toString()
             )
-    }
-
-    private fun getDateAndTimeOfMeeting() {
-        val year = datePickerCalendar.value!!.get(Calendar.YEAR)
-        val month = datePickerCalendar.value!!.get(Calendar.MONTH)
-        val day = datePickerCalendar.value!!.get(Calendar.DAY_OF_MONTH)
-        val hour = timePickerCalendar.value!!.get(Calendar.HOUR_OF_DAY)
-        val minute = timePickerCalendar.value!!.get(Calendar.MINUTE)
-
-        meetingInUtc.set(year, month, day, hour, minute)
-    }
-
-    private fun getCoordinate() {
-        val latitude = position.value!!.latitude
-        val longitude = position.value!!.longitude
-
-        location = GeoPoint(latitude, longitude)
     }
 
     fun createMeeting() {
@@ -126,11 +119,21 @@ class AddMeetingViewModel : BaseViewModel() {
         localDatabaseService.add("localMeetingsList", myMeetingsList)
     }
 
-    override fun onResume() {
-        val selectedDog = dataExchangeService.get<DogObj>(this::class.qualifiedName!!)
-        if (selectedDog != null) {
-            dog.value = selectedDog!!
-        }
+    private fun getDateAndTimeOfMeeting() {
+        val year = datePickerCalendar.value!!.get(Calendar.YEAR)
+        val month = datePickerCalendar.value!!.get(Calendar.MONTH)
+        val day = datePickerCalendar.value!!.get(Calendar.DAY_OF_MONTH)
+        val hour = timePickerCalendar.value!!.get(Calendar.HOUR_OF_DAY)
+        val minute = timePickerCalendar.value!!.get(Calendar.MINUTE)
+
+        meetingInUtc.set(year, month, day, hour, minute)
+    }
+
+    private fun getCoordinate() {
+        val latitude = position.value!!.latitude
+        val longitude = position.value!!.longitude
+
+        location = GeoPoint(latitude, longitude)
     }
 
     private fun isDogSelected(): Boolean {
@@ -144,5 +147,11 @@ class AddMeetingViewModel : BaseViewModel() {
     @SuppressLint("UseCompatLoadingForDrawables")
     private fun getDogPlaceHolder(): Drawable? {
         return activityService.activity!!.getDrawable(R.drawable.ic_dog_svgrepo_com)
+    }
+
+    private fun doesUserHaveOnlyOneDog(): Boolean {
+        if (localDatabaseService.get<ArrayList<DogObj>>("localDogsList")!!.size == 1)
+            return true
+        return false
     }
 }
