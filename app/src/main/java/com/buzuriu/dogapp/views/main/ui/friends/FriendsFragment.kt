b5515451f970970b8a9fcc3e270184e683526677
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2024-04-15 22:20:16 +0300

    Search bar added + read the searched text.

--- app/src/main/java/com/buzuriu/dogapp/views/main/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/buzuriu/dogapp/views/main/ui/friends/FriendsFragment.kt
@@ -1,18 +1,47 @@
 package com.buzuriu.dogapp.views.main.ui.friends
 
-import com.buzuriu.dogapp.R
+import android.R
+import android.os.Bundle
+import android.view.View
+import android.widget.SearchView
 import com.buzuriu.dogapp.databinding.FragmentFriendsBinding
 import com.buzuriu.dogapp.views.base.BaseBoundFragment
 
-class FriendsFragment : BaseBoundFragment<FriendsViewModel, FragmentFriendsBinding>(FriendsViewModel::class.java) {
+
+class FriendsFragment :
+    BaseBoundFragment<FriendsViewModel, FragmentFriendsBinding>(FriendsViewModel::class.java) {
 
     private lateinit var currentBinding: FragmentFriendsBinding
+    private lateinit var searchView: SearchView
 
     override fun setupDataBinding(binding: FragmentFriendsBinding) {
         binding.viewModel = mViewModel
         currentBinding = binding
     }
 
-    override val layoutId: Int = R.layout.fragment_friends
+    override val layoutId: Int = com.buzuriu.dogapp.R.layout.fragment_friends
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val currentSearchView =
+            getView()?.findViewById<SearchView>(com.buzuriu.dogapp.R.id.search_by_email)
+
+        if (currentSearchView != null) {
+            searchView = currentSearchView
+        }
+
+        searchView.setOnQueryTextListener(object :  SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                var searchedText  = query // here it's the text user has searched for
+                return false
+            }
+
+        })
+    }
 }
--- app/src/main/java/com/buzuriu/dogapp/views/main/ui/friends/FriendsViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/views/main/ui/friends/FriendsViewModel.kt
@@ -1,7 +1,15 @@
 package com.buzuriu.dogapp.views.main.ui.friends
 
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import com.buzuriu.dogapp.viewModels.BaseViewModel
 
 class FriendsViewModel : BaseViewModel() {
+
+    var searchedEmail = MutableLiveData<String>()
+
+    init {
+
+    }
 
 }
