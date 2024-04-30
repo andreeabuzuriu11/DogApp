cbb0c699e18b138a6b33a2e3292a52ad96bc638e
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2024-04-30 23:55:11 +0300

    Created request object.

---
+++ app/src/main/java/com/buzuriu/dogapp/models/RequestObj.kt
@@ -0,0 +1,17 @@
+package com.buzuriu.dogapp.models
+
+class RequestObj {
+    var ownRequests: ArrayList<String>? = null
+    var friendsRequests: ArrayList<String>? = null
+    var myFriends: ArrayList<String>? = null
+
+    constructor(
+        ownRequests: ArrayList<String>,
+        friendsRequests: ArrayList<String>,
+        myFriends: ArrayList<String>
+    ) {
+        this.ownRequests = ownRequests
+        this.friendsRequests = friendsRequests
+        this.myFriends = myFriends
+    }
+}
