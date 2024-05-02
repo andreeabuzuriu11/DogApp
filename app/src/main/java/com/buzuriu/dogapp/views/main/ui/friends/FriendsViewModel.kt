c6714da9afb0a92347928760fa308004b1449c6b
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2024-05-02 20:56:05 +0300

    Removed unused code from friends view model.

--- app/src/main/java/com/buzuriu/dogapp/views/main/ui/friends/FriendsViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/views/main/ui/friends/FriendsViewModel.kt
@@ -102,84 +102,19 @@ class FriendsViewModel : BaseViewModel() {
         println("Friends request cell pressed")
     }
 
-    fun findUser(searchedUserText: String) {
-//        todo fix loading
-//        showLoading(true)
-        viewModelScope.launch(Dispatchers.Main) {
-            val allUsers = databaseService.fetchUsers()
-            if (allUsers != null) {
-                for (user in allUsers)
-                    if (user.email!!.contains(searchedUserText))
-                        foundUser = user
-                    else
-                        if (user.name != null && user.name!!.contains(searchedUserText))
-                            foundUser = user
-            }
 
-            if (foundUser == null) {
-                return@launch
-            }
-
-        }
-
-
-        if (foundUser != null) {
-            if (!isUserAlreadyFound(foundUser!!)) {
-                foundUsersList.add(foundUser!!)
-                //todo fix loading
-//                showLoading(false)
-            }
-
-            if (!isUserMatchingWithCurrentSearch(foundUser!!, searchedUserText))
-                foundUsersList.remove(foundUser)
-        }
-
-        if (isSearchedTextEmpty(searchedUserText)) {
-            foundUsersList.removeAll(foundUsersList.toSet())
-            //todo fix loading
-//            showLoading(false)
-        }
-
-        userAdapter!!.notifyDataSetChanged()
-    }
-
-
-    private fun isUserAlreadyFound(userObj: UserObj): Boolean {
-        if (foundUsersList.contains(userObj))
-            return true
-        return false
-    }
-
-    private fun isUserMatchingWithCurrentSearch(
-        userObj: UserObj,
-        searchedUserText: String
-    ): Boolean {
-        if (userObj.email!!.contains(searchedUserText))
-            return true
-        if (userObj.name!!.contains(searchedUserText))
-            return true
-        return false
-    }
-
-    private fun isSearchedTextEmpty(searchedUserText: String): Boolean {
-        if (searchedUserText.length == 1 || searchedUserText == "")
-            return true
-        return false
-    }
-
-    fun showLoading(isVisible: Boolean) {
+    private fun showLoading(isVisible: Boolean) {
         showLoadingView(isVisible)
     }
 
-    fun showFriendProfile(userObj: UserObj) {
-        Log.d("click", "show friend prfile")
+    private fun showFriendProfile(userObj: UserObj) {
+        Log.d("click", "show friend profile")
     }
 
-    fun sendFriendRequest(userReceivingRequest: UserObj) {
+    private fun sendFriendRequest(userReceivingRequest: UserObj) {
         var userSendingRequestUid: String = firebaseAuthService.getCurrentUser()!!.uid
 
     }
-
 
     fun searchFriends() {
         navigationService.navigateToActivity(AddFriendActivity::class.java, false)
