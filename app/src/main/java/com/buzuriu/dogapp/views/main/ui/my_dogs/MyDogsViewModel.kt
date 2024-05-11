d2dfa8c32a0bbbcad8fb5d8772e71d7930f40d54
Andreea Buzuriu <andreeabuzuriu11@gmail.com>
2024-05-11 12:35:54 +0300

    Read csv data as list string.

--- app/src/main/java/com/buzuriu/dogapp/views/main/ui/my_dogs/MyDogsViewModel.kt
+++ app/src/main/java/com/buzuriu/dogapp/views/main/ui/my_dogs/MyDogsViewModel.kt
@@ -1,10 +1,13 @@
 package com.buzuriu.dogapp.views.main.ui.my_dogs
 
+import android.R
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import androidx.lifecycle.MutableLiveData
 import com.buzuriu.dogapp.adapters.DogAdapter
 import com.buzuriu.dogapp.listeners.IClickListener
 import com.buzuriu.dogapp.models.DogObj
+import com.buzuriu.dogapp.models.DogPersonality
 import com.buzuriu.dogapp.utils.LocalDBItems
 import com.buzuriu.dogapp.viewModels.BaseViewModel
 import com.buzuriu.dogapp.viewModels.DogDetailViewModel
@@ -12,6 +15,10 @@ import com.buzuriu.dogapp.views.AccountDetailActivity
 import com.buzuriu.dogapp.views.AddDogActivity
 import com.buzuriu.dogapp.views.DogDetailActivity
 import com.buzuriu.dogapp.views.auth.LoginActivity
+import com.opencsv.CSVReader
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import java.io.*
 
 
 @SuppressLint("NotifyDataSetChanged")
@@ -22,23 +29,105 @@ class MyDogsViewModel : BaseViewModel() {
     var doesUserHaveAnyDog = MutableLiveData(false)
 
     init {
-        val dogsFromLocalDB = localDatabaseService.get<ArrayList<DogObj>>(LocalDBItems.localDogsList)
+        val dogsFromLocalDB =
+            localDatabaseService.get<ArrayList<DogObj>>(LocalDBItems.localDogsList)
         if (dogsFromLocalDB != null && dogsFromLocalDB.size > 0) {
             dogsList.addAll(dogsFromLocalDB)
             doesUserHaveAnyDog.value = true
-        }
-        else
+        } else
             doesUserHaveAnyDog.value = false
 
         dogAdapter = DogAdapter(dogsList, ::selectedDog)
         dogAdapter!!.notifyDataSetChanged()
+
+        // read the csv
+        readCsv()
+    }
+
+    fun readCsv() {
+//        val bufferedReader = BufferedReader(FileReader(File("assets/akc-data-latest.csv")))
+//        val csvParser = CSVParser(bufferedReader, CSVFormat.DEFAULT);
+//        var bufferedReader =
+//            activityService.activity!!.applicationContext.assets.open("akc-data-latest.csv")
+//                .bufferedReader()
+//
+//        val csvParser = CSVParser(bufferedReader, CSVFormat.DEFAULT);
+
+        val result: MutableList<Array<String>> = mutableListOf()
+        val inputStream = activityService.activity!!.applicationContext.assets.open("akc-data-latest.csv")
+        val reader = InputStreamReader(inputStream)
+
+        // Initialize OpenCSV reader
+        val csvReader = CSVReader(reader)
+
+        // Read each line of the CSV file
+//        var line: Array<String>?
+//        while (csvReader.readNext().also { line = it } != null) {
+//            result.add(line!!)
+//        }
+
+        var line: Array<String>?
+        while (csvReader.readNext().also { line = it } != null) {
+            result.add(line!!)
+        }
+
+
+//        for (csvRecord in csvParser) {
+//            val breed = csvRecord.get(0);
+//            val description = csvRecord.get(1);
+//            val temperament = csvRecord.get(2);
+//            var popularity = csvRecord.get(3);
+//            var min_height = csvRecord.get(4);
+//            var max_height = csvRecord.get(5);
+//            var min_weight = csvRecord.get(6);
+//            var max_weight = csvRecord.get(7);
+//            var min_expectancy = csvRecord.get(8);
+//            var max_expectancy = csvRecord.get(9);
+//            var group = csvRecord.get(10);
+//            var grooming_frequency_value = csvRecord.get(11);
+//            var grooming_frequency_category = csvRecord.get(12);
+//            var shedding_value = csvRecord.get(13);
+//            var shedding_category = csvRecord.get(14);
+//            var energy_level_value = csvRecord.get(15);
+//            var energy_level_category = csvRecord.get(16);
+//            var trainability_value = csvRecord.get(17);
+//            var trainability_category = csvRecord.get(18);
+//            var demeanor_value = csvRecord.get(19);
+//            var demeanor_category = csvRecord.get(20);
+//            println(
+//                DogPersonality(
+//                    breed,
+//                    description,
+//                    temperament,
+//                    popularity,
+//                    min_height,
+//                    max_height,
+//                    min_weight,
+//                    max_weight,
+//                    min_expectancy,
+//                    max_expectancy,
+//                    group,
+//                    grooming_frequency_value,
+//                    grooming_frequency_category,
+//                    shedding_value,
+//                    shedding_category,
+//                    energy_level_value,
+//                    energy_level_category,
+//                    trainability_value,
+//                    trainability_category,
+//                    demeanor_value,
+//                    demeanor_category
+//                )
+//            );
+//        }
     }
 
     override fun onResume() {
         super.onResume()
         val isRefreshListNeeded: Boolean? =
             exchangeInfoService.get<Boolean>(this::class.qualifiedName!!)
-        val dogsFromLocalDB = localDatabaseService.get<ArrayList<DogObj>>(LocalDBItems.localDogsList)
+        val dogsFromLocalDB =
+            localDatabaseService.get<ArrayList<DogObj>>(LocalDBItems.localDogsList)
         if (isRefreshListNeeded != null && isRefreshListNeeded) {
             dogsList.clear()
 
@@ -46,8 +135,7 @@ class MyDogsViewModel : BaseViewModel() {
                 dogsList.addAll(dogsFromLocalDB)
                 doesUserHaveAnyDog.value = true
                 dogAdapter!!.notifyDataSetChanged()
-            }
-            else
+            } else
                 doesUserHaveAnyDog.value = false
 
         }
